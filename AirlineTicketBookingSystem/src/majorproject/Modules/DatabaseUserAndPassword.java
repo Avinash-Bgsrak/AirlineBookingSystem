e1e190ff7120932f590f0e0aa5a6c33b6505ebd0
Avinash Kumar <120415611+Avinash-Bgsrak@users.noreply.github.com>
2023-01-05 12:54:10 +0530

    Add files via upload

---
+++ AirlineTicketBookingSystem/src/majorproject/Modules/DatabaseUserAndPassword.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package majorproject.Modules;
+
+/**
+ *
+ * @author Avinash Kumar
+ */
+public class DatabaseUserAndPassword {
+    
+    String user = "root";
+    String pass = "root";
+}
